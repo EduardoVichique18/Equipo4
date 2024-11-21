440610cb647e8c82116f20b3b4428efbec4363f1
chema2323val <you@example.com>
2024-11-21 17:26:51 -0600

    Validaciones login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup; // Declaramos el formulario reactivo
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    // Inicializamos el formulario con validaciones
+    this.loginForm = this.formBuilder.group({
+      email: [
+        '',
+        [Validators.required, Validators.email], // Requerido y debe ser un correo válido
+      ],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(6)], // Requerido, mínimo 6 caracteres
+      ],
+    });
   }
 
+  ngOnInit() {}
+
+  // Método para manejar el inicio de sesión
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log('Formulario válido', this.loginForm.value);
+      // Aquí podrías añadir lógica para enviar datos al servidor
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
